e674640480c80789985f67ff6dec3d51a557b9bc
Mitesh Mankame <Mankame.Mitesh@azdeq.gov>
2019-09-27 10:10:40 -0700

    Changes for Okta logout

--- external/src/static/app/core/component/nav/mydeq-header/mydeq-header.component.ts
+++ external/src/static/app/core/component/nav/mydeq-header/mydeq-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Observable, forkJoin } from 'rxjs';
+
 interface HeaderDetails{
   first_name?:string,
   last_name?:string
@@ -50,10 +52,56 @@ interface HeaderDetails{
 export class MydeqHeaderComponent implements OnInit {
 
   @Input('headerDetail') headerDetail:HeaderDetails = {first_name:null,last_name:null};
-  constructor() { }
+  localStorageKey = 'visitedAppList';
+  myAppList: string[] = [];
+  observerList$: Observable<any>[] = [];
+  context: string;
+  
+  constructor() { 
+   this.context = window.location.pathname.substring(1, window.location.pathname.indexOf('/', 2));
+   const appList: string[] = this.getFromSessionStorage(this.localStorageKey);
+   this.myAppList = appList ? appList.filter(f => f !== this.context).concat([this.context]) : [this.context];
+   localStorage.setItem(this.localStorageKey, JSON.stringify(this.myAppList));
+  }
 
   ngOnInit() {
     
+  }
+  
+  logoutMe() {
+    this.myAppList.forEach(app => {     
+      if(app!=this.context){
+        this.observerList$.push(this.service.logoutMe(app));
+      }      
+    });
+
+    if(this.observerList$ && this.observerList$.length > 0){
+      forkJoin(this.observerList$).subscribe(
+        response => {
+          this.redirectToLogout();
+        },
+        error => {
+          this.redirectToLogout();
+        });
+    }
+    else{
+      this.redirectToLogout();
+    }
+    
+  }
+
+  getFromSessionStorage(key: string) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (e) {
+      console.error('Error getting data from sessionStorage', e);
+      return null;
+    }
+  }
+
+  redirectToLogout() {
+    window.open('logout', '_self');
+    localStorage.setItem(this.localStorageKey, JSON.stringify([]));
   }
 
 }
